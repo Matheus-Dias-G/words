580332f5114941944fb02a856dfa5f5f9226b3af
Matheus-Dias-G <neotromd@gmail.com>
2020-11-01 16:53:57 -0300

    main program

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+import time
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f',
+			'g', 'h', 'i', 'j', 'k', 'l',
+			'm', 'n', 'o', 'p', 'q', 'r',
+			's', 't', 'u', 'v', 'w', 'y',
+			'z']
+word = str(input('type anything: '))
+
+
+x = 0
+y = 0
+v = False
+words = []
+while v == False:
+	comp_word = ''.join(words)
+	print(comp_word, alphabet[y])
+	time.sleep(0.5)
+	os.system('cls' if os.name == 'nt' else 'clear')
+	if word[x] == alphabet[y]:
+		words.append(alphabet[y])
+		x += 1
+		y = 0
+	else:
+		y += 1
+	if x > len(word)-1:
+		comp_word = ''.join(words)
+		print(comp_word)
+		break
