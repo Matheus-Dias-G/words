8aafc033f7ae2faad5b95d378a04234ccc0d041e
Matheusdias <neotromd@gmail.com>
2020-11-01 20:59:03 -0300

    Fixing errors

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import os
 import time
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f',
-			'g', 'h', 'i', 'j', 'k', 'l',
-			'm', 'n', 'o', 'p', 'q', 'r',
-			's', 't', 'u', 'v', 'w', 'y',
-			'z']
-word = str(input('type anything: '))
+            'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r',
+	    's', 't', 'u', 'v', 'w', 'y',
+	    'z']
+word = str(input('type a word: '))
 
 
 x = 0
